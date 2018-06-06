b8c2b4b4aa560b70fc5b9310c27f2f67b1d53616
Irving Herrera <irving.herreramolina@gmail.com>
2018-06-06 16:32:52 -0500

    filtro de busqueda

--- Sistema/Sistema/Controllers/CategoriasController.cs
+++ Sistema/Sistema/Controllers/CategoriasController.cs
@@ -19,12 +19,18 @@ namespace Sistema.Controllers
         }
 
         // GET: Categorias
-        public async Task<IActionResult> Index(string sortOrder)
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
         {
             ViewData["NombreSortParm"] = string.IsNullOrEmpty(sortOrder) ? "nombre_desc" : string.Empty;
             ViewData["DescripcionSortParm"] = sortOrder == "descripcion_asc" ? "descripcion_desc" : "descripcion_asc";
+            ViewData["CurrentFilter"] = searchString;
 
             var categoria = from s in _context.Categoria select s;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                categoria = categoria.Where(s => s.Nombre.Contains(searchString) || s.Descripcion.Contains(searchString));
+            }
 
             switch (sortOrder)
             {
