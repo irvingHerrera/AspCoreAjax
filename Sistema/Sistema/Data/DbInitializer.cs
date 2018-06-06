70b5ca71a2def1ace57f93abd5e0cdae83bfc559
Irving Herrera <irving.herreramolina@gmail.com>
2018-06-06 14:50:52 -0500

    cambio de equitas en las vistas

---
+++ Sistema/Sistema/Data/DbInitializer.cs
@@ -0,0 +1,43 @@
+﻿using Sistema.Models;
+using System.Linq;
+
+namespace Sistema.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SistemaContext context)
+        {
+            context.Database.EnsureCreated();
+
+            //Buscar si existe registros en la tabla categoria
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+
+            var categorias = new Categoria[]
+                {
+                    new Categoria
+                    {
+                        Nombre = "Programacion",
+                        Descripcion = "Curso de Programacion",
+                        Estado = true
+                    },
+                    new Categoria
+                    {
+                        Nombre = "Diseño Grafico",
+                        Descripcion = "Curso de Diseño Grafico",
+                        Estado = true
+                    }
+                };
+
+            foreach (var item in categorias)
+            {
+                context.Categoria.Add(item);
+            }
+
+            context.SaveChanges();
+
+        }
+    }
+}
--- Sistema/Sistema/Startup.cs
+++ Sistema/Sistema/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
+using Sistema.Data;
 using Sistema.Models;
 
 namespace Sistema
@@ -41,7 +37,7 @@ namespace Sistema
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SistemaContext context)
         {
             if (env.IsDevelopment())
             {
@@ -63,6 +59,8 @@ namespace Sistema
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DbInitializer.Initialize(context);
         }
     }
 }
